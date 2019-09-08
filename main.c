8c34b3d766572f48c367ed66ba57f76a5913f5cc
Gabriel Badilla <gabriel.badilla.alfaro@gmail.com>
2019-09-08 17:29:00 -0300

    Fix subtract function.

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 /* Laboratory Advance Programming */
 /* Authors: Gabriel Badilla & Rodrigo Kobayashi */
 /* Compiler: gcc */
-#include "doublyLinkedListPolynomial.h"
 #include "basicPolynomialArithmetics.h"
+#include "doublyLinkedListPolynomial.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
   int n;
@@ -18,15 +17,15 @@ int main() {
   struct Polynomial *copyPoly = copyPolynomial(polynomial);
   printf("\nPolynomial copy:\n");
   printPolynomial(copyPoly->head);
-  //Second copy.
+  // Second copy.
   struct Polynomial *copy2Poly = copyPolynomial(polynomial);
 
-  printf("\nPolynomial substraction:\n");
-  struct Polynomial* substract = substractPolynomials(polynomial, copyPoly);
-  printPolynomial(substract->head);
+  printf("\nPolynomial subtraction:\n");
+  struct Polynomial *subtract = subtractPolynomials(polynomial, copyPoly);
+  printPolynomial(subtract->head);
 
   printf("\nPolynomial sum:\n");
-  struct Polynomial* sum = addPolynomials(polynomial, copy2Poly);
+  struct Polynomial *sum = addPolynomials(polynomial, copy2Poly);
   printPolynomial(sum->head);
 
   freeList(polynomial);
